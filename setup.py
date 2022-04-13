5f8e4709f593beee7feddb208d6016a5a4e2e97d
sanja <sanja@ebi.ac.uk>
2022-04-13 15:04:04 +0100

    Added __init__.py and setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='emdb_schemas',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='sanja',
+    author_email='sanja@ebi.ac.ik',
+    description='EMDB schema files',
+    zip_safe=False
+)
