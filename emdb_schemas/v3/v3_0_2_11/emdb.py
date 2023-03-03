0fb7e9928c7eecc2de928051f5ac407b90d222f1
amudha <amudha@ebi.ac.uk>
2023-03-03 09:25:20 +0000

    new items in _em_3d_fitting_list

--- emdb_schemas/v3/v3_0_2_11/emdb.py
+++ emdb_schemas/v3/v3_0_2_11/emdb.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Mon Feb 13 09:32:06 2023 by generateDS.py version 2.29.5.
+# Generated Thu Feb 23 08:28:44 2023 by generateDS.py version 2.29.5.
 # Python 2.7.15 | packaged by conda-forge | (default, Mar  5 2020, 14:58:04)  [GCC Clang 9.0.1 ]
 #
 # Command line options:
